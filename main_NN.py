8c4d9c562942dd9b07b05d73cbe3b4e038e7c1e1
Matvey Popov <matvey.popov@innowise-group.com>
2023-02-10 00:16:41 +0300

    first version of a model

--- main_NN.py
+++ main_NN.py
@@ -3,7 +3,6 @@ import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error
-from sklearn.metrics import accuracy_score
 
 label_encoder = LabelEncoder()
 
