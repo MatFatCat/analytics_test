e049aabcd34660709110a9539662fb6aade2e2b7
Matvey Popov <matvey.popov@innowise-group.com>
2023-02-12 01:42:51 +0300

    second version of model with 2 1-hot features

--- sand.py
+++ sand.py
@@ -1,28 +1,22 @@
+import time
+import requests as req
 import pandas as pd
-
-PRODUCT_TYPES = ["ТВОРОГ", "СЫР", "МОЛОКО", "ЙОГУРТ", "МОРОЖЕНОЕ", "МАСЛО", "СЛИВКИ",
-                 "КЕФИР", "СМЕТАНА"]
-
-df = pd.read_csv("/Users/matthewpopov/Desktop/clean_database/dataset.csv", encoding="utf-8")
-
-milk_products = pd.read_csv("/Users/matthewpopov/Desktop/clean_database/milk_catalogs_items.csv", encoding="utf-8")
-
-def row_handler(row):
-    global PRODUCT_TYPES
-    item_id = row["item_id"]
-    product_row = milk_products.loc[milk_products["id"] == item_id]
-    product_name = product_row["name"].tolist()[0]
-    product_type = product_name.split()[0].upper()
-    if product_type in PRODUCT_TYPES:
-        return product_type
-    else:
-        return "ДРУГОЕ"
+from datetime import datetime
 
 
-del df[df.columns[0]]
+def row_handler(row): return datetime.strptime(row["date"], '%Y-%m-%d').weekday()
 
-df["product_type"] = df.apply(lambda row: row_handler(row), axis=1)
 
-del df[df.columns[2]]
+PATH = "/Users/matthewpopov/Desktop/clean_database/dataset.csv"
 
-df.to_csv("/Users/matthewpopov/Desktop/clean_database/dataset_types.csv")
+df = pd.read_csv(PATH, encoding="utf-8")
+
+df = df.drop(["Unnamed: 0", "Unnamed: 0.1"], axis=1)
+
+print(df)
+
+df["day_of_week_number"] = df.apply(lambda row: row_handler(row), axis=1)
+
+print(df)
+
+df.to_csv("/Users/matthewpopov/Desktop/clean_database/dataset.csv")
